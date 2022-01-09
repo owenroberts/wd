530319df872eeeb7029ca857811cebd46de208b5
owenroberts <obroberts@gmail.com>
2022-01-08 19:36:53 -0500

    css, text fixes, more tet

--- src/window.js
+++ src/window.js
@@ -35,71 +35,73 @@ gme.load({
 	items: './data/items.json'
 }, false);
 
-let clickOne, leftLabel, rightLabel;
+let clickOne, leftLabel, rightLabel, textY;
+let wrap = 10, alphaString = 'abcdefghijklmnopqrstuvwxyz';
 
 let data = {
 	window: { next: ['tree', 'bird'],
-		label: ['window', 'setting', ]
+		label: ['window', 'setting',  'in the beginning', 'the trouble', 'back to the beginning']
 	},
 	door: { next: ['cat', 'dog'],
-		label: ['door', 'exposition', ]
+		label: ['door', 'exposition', 'it was a dark room', 'they heard a sound', 'their popularity had passed', 'the entrance closed behind them']
 	},
 	bird: { next: ['flower', 'rabbit'],
-		label: ['bird', 'inciting incident']
+		label: ['bird', 'inciting incident', 'a bantam chicken', 'the humble proletarian', 'the fowl']
 	},
 	cat: { next: ['jar', 'plunger'],
-		label: ['cat', 'protagonist']
+		label: ['cat', 'protagonist', 'they were lost', 'they smelled', 'they were in hell', 'they came with tooth, bone and pearl']
 	},
 	dog: { next: ['hole', 'mailbox'],
-		label: ['dog', 'character']
+		label: ['dog', 'character', 'they must fetch', 'fetch a fellow', 'an associate', 'they found the fellow']
 	},
 	tree: { next: ['serpent', 'fruit'],
-		label: ['tree', 'climax']
+		label: ['tree', 'climax', 'the hunt began', 'they are the player', 'they found the seed']
 	},
 	jar: { next: ['piss_christ', 'quorum'],
-		label: ['jar', 'resolution',]
+		label: ['jar', 'resolution', 'they were a thinker', 'they find a jar of memories', 'they were lying', ]
 	},
 	employer: { next: ['nail', 'tofu'],
-		label: ['employer', 'gauntlet',]
+		label: ['employer', 'gauntlet', 'a true leader', 'a piece of shit', 'make more money', 'die alone']
 	},
 	flower: { next: ['garbage', 'valley'],
-		label: ['flower', 'time cave',]
+		label: ['flower', 'time cave', 'periodic', 'they fall in love', 'they smell in love', 'they grow together', 'they wilt away']
 	},
 	fruit: { next: ['pig', 'premise'],
-		label: ['fruit', 'branch',]
+		label: ['fruit', 'branch', 'they did not understand the consequence', 'the taste was unforgettable', ]
 	},
 	garbage: { next: ['napkin', 'employer'],
-		label: ['garbage', 'bottleneck',]
+		label: ['garbage', 'bottleneck', 'they lived in a wasteland', 'they survived on scraps', 'it all turned out okay']
 	},
 	hole: { next: ['iced_coffee', 'oyster'],
-		label: ['hole', 'quest',]
+		label: ['hole', 'quest', 'they thought it could be fixed', 'they appealed to their persecutor', 'they were in a hole' ]
 	},
 	iced_coffee: { next: ['vacuum', 'moon'],
-		label: ['iced_coffee', 'conflict'] 
+		label: ['iced coffee', 'conflict', 'they could not find an iced coffee anywhere', 'a savior came', 'their blood flowed from their veins', ] 
 	},
 	kill: { next: ['leaf', 'shirt'],
-		label: ['kill', 'sorting hat', ]
+		label: ['kill', 'turning point', 'they did a crime', 'they pursed the criminal', 'vengeance']
 	},
 	leaf: { next: ['worm', 'rubric'],
-		label: ['leaf', 'floating', ]
+		label: ['leaf', 'floating', 'the garment was a clue', 'it had to be their family', 'but it could not be their family']
 	},
 	mailbox: { next: ['nacho', 'quicksand'],
-		label: ['mailbox', 'loop and grow', ]
+		label: ['mailbox', 'loop and grow', 'the letter arrived in the mailbox', 'they saw through the foliage', 'the letter foretold a disaster']
 	},
 	moon: { next: ['kill', 'window'],
-		label: ['moon', 'flashback', ]
+		label: ['moon', 'flashback', 'mindless cruelty', 'why', 'why did they do it']
 	},
 	nacho: { next: ['door', 'underpants'],
-		label: ['nacho', 'fishbone', ]
+		label: ['nacho', 'fishbone', 'there was a tyrant', 'and a conspiracy', 'but the plot failed']
 	},
 	nail: { next: ['jar', 'bird'],
-		label: ['nail', 'parallel', ]
+		label: ['nail', 'parallel', 'they were tagged in the code', 'the enemy was at the grocery store', 'the object was their to take']
 	},
 	napkin: { next: ['cat', 'door'],
-		label: ['napkin', 'frame', ]
+		label: ['napkin', 'frame', 'an abduction', 'they could not be found']
 	},
+
 	oyster: { next: ['dog', 'employer'],
-		label: ['oyster', 'progress',]
+		label: ['oyster', 'progress', 'an explosion']
 	},
 	pig: { next: ['flower', 'fruit'],
 		label: ['pig', 'emergence', ]
@@ -157,15 +159,9 @@ function newScene(a, b) {
 	gme.scenes.current = sceneName;
 
 	let [leftLabelText, rightLabelText] = [left, right].map(side => {
-		if (data[side].index === undefined) {
-			data[side].index = 0;
-			return data[side].label[data[side].index];
-		} else if (data[side].index < data[side].label.length - 1) {
-			data[side].index++;
-			return data[side].label[data[side].index];
-		} else { 
-			return 'the end';
-		}
+		return data[side].index < data[side].label.length - 1 ?
+			data[side].label[data[side].index]:
+			'the end';
 	});
 
 	leftLabel.setMsg(leftLabelText);
@@ -173,6 +169,9 @@ function newScene(a, b) {
 
 	leftLabel.x = Math.round(gme.view.width * 0.25);
 	rightLabel.x = Math.round(gme.view.width * 0.75);
+
+	leftLabel.y = textY;
+	rightLabel.y = textY;
 
 	leftLabel.center();
 	rightLabel.center();
@@ -211,16 +210,17 @@ function newScene(a, b) {
 			};
 
 			ui.onClick = function() {
+
 				if (!clickOne.clickedOnce) clickOne.clickedOnce = true;
 				ui.animation.cancelOverride();
-				if (data[ui.item]) newScene(...data[ui.item].next);
+				if (data[ui.item]) {
+					newScene(...data[ui.item].next);
+					data[ui.item].index++;
+				}
 			};
 		});
 	}
-
-	
 }
-
 
 gme.start = function() {
 	document.getElementById('splash').remove();
@@ -231,20 +231,25 @@ gme.start = function() {
 	clickOne.clickedOnce = false;
 	gme.scenes.start.addToDisplay(clickOne);
 
-	let y = 0.8 * gme.view.height
+	textY = 0.8 * gme.view.height;
 	let letters = gme.anims.ui.alphabet;
-	let alphaString = 'abcdefghijklmnopqrstuvwxyz';
-	leftLabel = new Text(gme.view.halfWidth / 2, y, 'window', 8, letters, alphaString);
-	rightLabel = new Text(0.75 * gme.view.width, y, 'door', 8, letters, alphaString);
+	
+	leftLabel = new Text(gme.view.halfWidth / 2, textY, '', wrap, letters, alphaString);
+	rightLabel = new Text(0.75 * gme.view.width, textY, '', wrap, letters, alphaString);
 	leftLabel.center();
 	rightLabel.center();
 
-	let test = new Text(gme.view.halfWidth, gme.view.halfHeight, 'inciting incident', 8, letters, alphaString)
+	for (const k in data) {
+		data[k].index = 0;
+	}
+
+	test = new Text(gme.view.halfWidth, gme.view.halfHeight, 'they find a jar of memories', wrap, letters, alphaString);
+
 	newScene('window', 'door');
 };
 
-
 gme.draw = function() {
+	// test.display();
 	if (!clickOne.clickedOnce) clickOne.display();
 	gme.scenes.current.display();
 	leftLabel.display(false, false, 0, 0, true);
@@ -261,4 +266,35 @@ gme.mouseDown = function(x, y) {
 
 gme.mouseUp = function(x, y) {
 	gme.scenes.current.mouseUp(x, y);
+};
+
+/* css stuff */
+
+const darkStyleCheckbox = document.getElementById('dark-style');
+const styleLabel = document.getElementById('style-label');
+
+darkStyleCheckbox.onchange = function() {
+	if (darkStyleCheckbox.checked) {
+		document.body.classList.add('dark');
+	} else {
+		document.body.classList.remove('dark');
+	}
+
+	let [c1, c2] = darkStyleCheckbox.checked ?
+		['#000000', '#ffffff'] :
+		['#ffffff', '#000000'];
+
+	for (const k in gme.anims.items) {
+		let a = gme.anims.items[k];
+		a.layers.forEach(l => {
+			if (l.color === c1) l.color = c2;
+		});
+	}
+
+	for (const k in gme.anims.ui) {
+		let a = gme.anims.ui[k];
+		a.layers.forEach(l => {
+			if (l.color === c1) l.color = c2;
+		});
+	}
 };
